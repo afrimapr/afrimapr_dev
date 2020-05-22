c609e888b4a25b4c91cea92732403e22ec404971
AndySouth <southandy@gmail.com>
2020-05-22 17:49:22 +0100

    add facility type conversion

--- create_facility_type_lookup.R
+++ create_facility_type_lookup.R
@@ -7,3 +7,31 @@ lookup_tb <- read_sheet("https://docs.google.com/spreadsheets/d/1esITXJxb2ph63sA
 
 # Save as .rda
 save(lookup_tb, file = "data/facility_type_lookup.rda" )
+
+#initial attempt at lookup with shortnames
+library(afrihealthsites)
+
+fts <- lookup_tb
+
+names(fts) <- c('country','type_who','type_new')
+
+
+sftogo <- afrihealthsites::afrihealthsites('togo', datasource = 'who', plot=FALSE)
+
+#sftogo$`Facility type`
+
+#match returns a vector of the positions of (first) matches of its first argument in its second.
+
+#cool this works
+sftogo$facility_type <- fts$type_new[ match(sftogo$`Facility type`,fts$type_who) ]
+
+#test for whole dataset just to check it doesn't take ages
+sfall <- afrihealthsites::afrihealthsites('all', datasource = 'who', plot=FALSE)
+
+#it's super quick :-)
+sfall$facility_type <- fts$type_new[ match(sfall$`Facility type`,fts$type_who) ]
+
+#tomorrow work out how best to put this in afrihealthsites
+#can add the column to the data stored df_who_sites 
+
+
