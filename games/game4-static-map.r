08956eca5b276507d77f5585f6d599980276231f
AndySouth <southandy@gmail.com>
2020-11-15 17:33:13 +0000

    country choosing roughly working

--- games/game4-static-map.r
+++ games/game4-static-map.r
@@ -11,16 +11,26 @@
 #starting from a more basic example
 #https://shiny.rstudio.com/gallery/plot-interaction-selecting-points.html
 
+#TODO i think calculation is triggered twice when mouse clicked
+#(possibly because updating the map retriggers click)
+
+#working that list of countries built up from clicks
+
+#next steps
+# add random generation of country names
+# check whether click is chosen country
+# add timer
+
+
 library(shiny)
-library(ggplot2)
+#library(ggplot2)
 library(afrilearndata)
 library(sf)
 
 
-
 ui <- fluidPage(
   fluidRow(
-    column(width = 4,
+    column(width = 12,
            plotOutput("plot1", height = 300,
                       # Equivalent to: click = clickOpts(id = "plot_click")
                       click = "plot1_click"
@@ -40,6 +50,9 @@ server <- function(input, output) {
   
   # store the list of clicked polygons in a vector
   clickedpolys <- shiny::reactiveValues( ids = vector() )
+  # index of next country for user to click
+  country_id_next <- 1
+  clicked_country <- ""
   
   output$plot1 <- renderPlot({
     
@@ -78,17 +91,20 @@ server <- function(input, output) {
 
   })
   
+  # this was orginally to print click info into a box
+  # i'm kind of using for something else
+  # to detect which country has been clicked
   output$click_info <- renderPrint({
-    input$plot1_click
+    #input$plot1_click
     #c(input$plot1_click$x,input$plot1_click$y)
     
-    #if( !is.null( input$plot1_click ) ){
+    if( !is.null( input$plot1_click ) ){
 
-    if( is.null( input$plot1_click ) ){
-        # check for required values, if true, then the issue
-        # is "silent". See more at: ?req
-        req( input$plot1_click )
-      } else {      
+    # if( is.null( input$plot1_click ) ){
+    #     # check for required values, if true, then the issue
+    #     # is "silent". See more at: ?req
+    #     req( input$plot1_click )
+    #   } else {      
       
       sfpoint <- st_sfc(st_point(c(input$plot1_click$x, input$plot1_click$y)))
       st_crs(sfpoint) <- 4326
@@ -101,13 +117,25 @@ server <- function(input, output) {
       clicked_country <- sfafricountries$name[poly_index]
 
       # store clicked country if it hasn't been clicked before
-      if ( ! (poly_index %in% clickedpolys$ids))
+      #if ( ! (poly_index %in% clickedpolys$ids))
+      
+      # store clicked country if it is correct
+      # (might no longer be necessary to check existing list as above)
+      if ( poly_index == country_id_next &
+           ! (poly_index %in% clickedpolys$ids) )  
       {
-        clickedpolys$ids <- c( clickedpolys$ids, poly_index )        
+        clickedpolys$ids <<- c( clickedpolys$ids, poly_index )  
+        
+        # change next country to choose
+        # later make this random, for now increment
+        country_id_next <<- country_id_next + 1
       }
-
-      #c(poly_index, clicked_country)
     }
+
+    #c(poly_index, clicked_country)
+    cat("target:",sfafricountries$name[country_id_next], 
+        " clicked:", clicked_country,
+        " all clicked:", clickedpolys$ids)  
     
   })
   
